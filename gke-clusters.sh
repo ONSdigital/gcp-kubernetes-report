e2db3f4f2658394640b2fec03267d21c4e2d93df
John Topley <john.topley@ons.gov.uk>
2019-06-12 14:16:07 +0100

    Initial commit

---
+++ gke-clusters.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p reports
+cd reports
+
+output_file=gke-clusters-`date +%Y-%m-%d-%H-%M-%S`.csv
+echo "Project,Name,Description,Master Version,Node Version,Node Count,Machine Type,Creation Date,Cluster CIDR,Services CIDR,Status" > $output_file
+
+for project_id in $(gcloud projects list --format="value(projectId)" | sort)
+do
+  cluster_data=`gcloud container clusters list --format="csv[no-heading](name,\
+                                                                         description,\
+                                                                         currentMasterVersion,\
+                                                                         currentNodeVersion,\
+                                                                         currentNodeCount,\
+                                                                         nodeConfig.machineType,\
+                                                                         createTime,\
+                                                                         clusterIpv4Cidr,\
+                                                                         servicesIpv4Cidr,\
+                                                                         status)" --project=$project_id`
+
+  if [ ! -z "$cluster_data" ]; then
+    echo $project_id,$cluster_data >> $output_file
+  fi
+done
