9bccf4f44506c5aaf9fcf6d42e701fb13774fc52
jedimasterjonny <46541898+jedimasterjonny@users.noreply.github.com>
2019-06-12 14:35:08 +0100

    Remove legacy back ticks

--- gke-clusters.sh
+++ gke-clusters.sh
@@ -4,12 +4,12 @@ set -e
 mkdir -p reports
 cd reports
 
-output_file=gke-clusters-`date +%Y-%m-%d-%H-%M-%S`.csv
+output_file=gke-clusters-$(date +%Y-%m-%d-%H-%M-%S).csv
 echo "Project,Name,Description,Master Version,Node Version,Node Count,Machine Type,Creation Date,Cluster CIDR,Services CIDR,Status" > $output_file
 
 for project_id in $(gcloud projects list --format="value(projectId)" | sort)
 do
-  cluster_data=`gcloud container clusters list --format="csv[no-heading](name,\
+  cluster_data=$(gcloud container clusters list --format="csv[no-heading](name,\
                                                                          description,\
                                                                          currentMasterVersion,\
                                                                          currentNodeVersion,\
@@ -18,7 +18,7 @@ do
                                                                          createTime,\
                                                                          clusterIpv4Cidr,\
                                                                          servicesIpv4Cidr,\
-                                                                         status)" --project=$project_id`
+									 status)" --project=$project_id)
 
   if [ ! -z "$cluster_data" ]; then
     echo $project_id,$cluster_data >> $output_file
